56b1ec42ba597bb013e3bbf7faeb2ae80f053d38
Jeffery <jpastvait@gmail.com>
2022-01-13 17:40:08 -0500

    updating file structure

--- server.js
+++ server.js
@@ -1,42 +1,29 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const apiRoutes = require("./routes/api-routes");
-const htmlRoutes = require("./routes/html-routes");
 const logger = require("morgan");
-const db = require("./models");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(apiRoutes);
-app.use(htmlRoutes);
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  }
-);
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
-const connection = mongoose.connection;
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
-// if successful mongoose connection
-connection.on("connected", () => {
-  console.log("Mongoose connected successfully.");
-});
-
-// if unsuccessful mongoose connection
-connection.on("error", (err) => {
-  console.log("Mongoose connected error:" + err);
-});
-
-// listen on the PORT
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
